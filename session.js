4762cdca79141b63979b3b3c715cc584398899bb
PASIYA12-del <pl7056256@gmail.com>
2025-02-11 23:39:13 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,6 +40,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your session id here",
+SESSION_ID: process.env.SESSION_ID || ANJU-XPRO~LZNHGIaT#vRUBD0vDY9yyXV2gxEsfw-FqKQqDmwaTfkk6VCEigtI",
 PORT: process.env.PORT || "8000"
 };
